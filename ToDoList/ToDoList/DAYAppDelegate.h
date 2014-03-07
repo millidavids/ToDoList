ddbe5d6c567be573be9f5980a1286369adbe24fc
David Yurek <david.a.yurek@gmail.com>
2014-03-07 11:29:37 -0500

    initial commit of app

---
+++ ToDoList/ToDoList/DAYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DAYAppDelegate.h
+//  ToDoList
+//
+//  Created by David Yurek on 3/7/14.
+//  Copyright (c) 2014 David Yurek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by David Yurek on 3/7/14.
+//  Copyright (c) 2014 David Yurek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DAYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DAYAppDelegate class]));
+    }
+}
